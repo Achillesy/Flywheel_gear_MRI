af3a9d645dec86f6478f29d4c1a8206fb7247443
HP-Z4-G4-Ubuntu <xuchu_liu@rush.edu>
2022-02-02 22:10:01 -0600

    list session file

--- Docker/unzip_dicom.py
+++ Docker/unzip_dicom.py
@@ -6,6 +6,7 @@
 #
 # \author     Xuchu Liu (xuchu_liu@rush.edu)
 # \date       2/2/2021
+import os
 import sys
 import logging
 
@@ -34,18 +35,35 @@ if __name__ == '__main__':
         context.log_config()
 
         # Check to make sure we have a valid destination container for this gear.
-        destination_level = context.destination.get("type")
-        if destination_level is None:
-            log.error(f"invalid destination {destination_level}")
+        cur_dest = context.destination
+        cur_dest_level = cur_dest.get("type")
+        if cur_dest_level is None:
+            log.error(f"invalid destination {cur_dest_level}")
             raise Exception("Invalid gear destination")
 
         # Get the destination group/project
-        destination = context.destination
-        output_dir = context.output_dir
+        fw = flywheel.Client(api_key)
+        dest_handle = fw.get(cur_dest['id'])
+        # log.debug(dest_handle)
+        # session_id = fw.get_project(dest_handle.parents.session)
+        cur_session = fw.get_session(dest_handle.parents.session)
+        log.debug(f'working in session {cur_session.label}')
 
-        log.debug(destination)
-        log.debug(output_dir)
-
+        # destination = context.get_destination_parent() # Will be session if analysis is run at session as is default
+        acquisitions = cur_session.acquisitions()
+        # work_dir = context.work_dir
+        work_dir = "/tmp/DZIP"
+        for acquisition in acquisitions:
+            # Assume only one file per acquisition, could filter for dicom files if needed
+            file_ = acquisition.files[0]
+            # Create a destinatino directory for this acqusitions file
+            acq_dir = os.path.join(work_dir, acquisition.label)
+            os.makedirs(acq_dir)
+            # Download file
+            acq_file = os.path.join(acq_dir, file_.name)
+            log.debug(acq_file)
+            #file_.download(acq_file)
+            # Unzip if necessary
     except Exception as e:
         log.exception(e)
         exit_status = 1
